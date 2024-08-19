dc9dacfb12c9b639cfddb5a0aa500ffd5c7f1215
rokon <ismail123ismail200@gmail.com>
2024-08-19 08:38:36 +0600

    first commit

--- src/Compones/Pages/Service.jsx
+++ src/Compones/Pages/Service.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Services from './Services';
 
 const Service = () => {
     const data = useLoaderData()
     console.log(data)
     return (
-        <div>
+        <div className='my-10'>
+            <h1 className='text-center text-3xl text-sky-400 font-bold items-center'> {data.hospital_name}</h1>
 
+            <div className='flex justify-around items-center'>
+                <div className='space-y-2 font-semibold text-lg '>
+                    <p>{data.address.street}</p>
+                    <p>{data.address.city}</p>
+                    <p>{data.address.zip_code}</p>
+                </div>
+                <div className='space-y-2 font-semibold text-lg '>
+                    <p>{data.phone_number}</p>
+                    <p>{data.email}</p>
+                </div>
+
+            </div>
+            <div>
+                {
+                    data.services?.map(service => <Services key={service} service={service}></Services>)
+                }
+            </div>
         </div>
     );
 };
---
+++ src/Compones/Pages/Services.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Services = ({service}) => {
+    console.log(service)
+    return (
+        <div>
+            <p>ddd{service.id}</p>
+
+            <div className=''>
+
+            </div>
+        </div>
+    );
+};
+
+export default Services;
